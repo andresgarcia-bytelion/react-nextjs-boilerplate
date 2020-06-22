a783914f61a48245e4e144d1f7ff0cb655f72959
Bryan <bryan.stoner@bytelion.com>
2020-06-22 17:11:32 -0400

    add example environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+require('dotenv').config({ path: '.env' });
+
 const withPlugins = require('next-compose-plugins');
 const withSvgr = require('next-svgr');
 
-module.exports = withPlugins([withSvgr]);
+module.exports = withPlugins([[withSvgr]], {
+  serverRuntimeConfig: {},
+  publicRuntimeConfig: {},
+  env: {},
+  poweredByHeader: false,
+});
