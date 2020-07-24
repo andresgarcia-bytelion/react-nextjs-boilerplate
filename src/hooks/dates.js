83102546dac02461e9ee2820c0d760791fe61866
Bryan <bryan.stoner@bytelion.com>
2020-07-24 17:09:27 -0400

    add hook to format date for inputs and add uuid package

--- src/hooks/dates.js
+++ src/hooks/dates.js
@@ -4,4 +4,6 @@ const dateFormat = 'M/D/YYYY';
 
 export const formatDate = (date) => moment(date).format(dateFormat);
 
+export const formatDateInput = (date) => moment(date).format('YYYY-MM-DD');
+
 export const formatDateTimestamp = (date) => moment.unix(date).utc().format(dateFormat);
