5b0e11f1cfaba24edaa826c19da42a759af2f057
Stoner-Bytelion <bryan.stoner@bytelion.com>
2021-04-07 08:11:02 -0400

    table base

---
+++ src/components/table.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const tableStyles = makeStyles({
+  root: {},
+});
+
+const Table = ({ headers, label, rows }) => {
+  const tableClasses = tableStyles();
+
+  return (
+    <TableContainer>
+      <Table aria-label={label}>
+        <TableHead>
+          <TableRow>
+            {headers.map((header) => (
+              <TableCell>{header}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.carbs}</TableCell>
+              <TableCell>{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+});
+
+export default CustomLink;
